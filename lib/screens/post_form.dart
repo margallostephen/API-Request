df64444f7e153f13e1bbea1a416e58254019fbf7
margallostephen <spmargallo@gmail.com>
2023-06-01 01:58:18 +0800

    Add form

---
+++ lib/screens/post_form.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../components/app_bar.dart';
+import '../components/style.dart';
+
+class PostForm extends StatefulWidget {
+  const PostForm({super.key});
+
+  @override
+  State<PostForm> createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+  dynamic arguments;
+  IconData? icon;
+
+  final taskFormKey = GlobalKey<FormState>();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController bodyController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    arguments = ModalRoute.of(context)?.settings.arguments;
+    arguments['operation'] == "Add Post" ? icon = Icons.add : icon = Icons.edit;
+
+    return Scaffold(
+      backgroundColor: Style.violet,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              CustomAppBar(title: arguments['operation'], icon: icon),
+              const SizedBox(
+                height: 20,
+              ),
+              Form(
+                key: taskFormKey,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(20, 25, 20, 25),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: titleController,
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Style.violet,
+                          ),
+                          labelText: 'Title',
+                          prefixIcon: Icon(Icons.topic, color: Style.violet),
+                          prefixIconColor: Style.violet,
+                          border: Style.normal,
+                          enabledBorder: Style.normal,
+                          focusedBorder: Style.focused,
+                          focusedErrorBorder: Style.errorFocused,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: titleController,
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Style.violet,
+                          ),
+                          labelText: 'Body',
+                          prefixIcon: Icon(Icons.article, color: Style.violet),
+                          prefixIconColor: Style.violet,
+                          border: Style.normal,
+                          enabledBorder: Style.normal,
+                          focusedBorder: Style.focused,
+                          focusedErrorBorder: Style.errorFocused,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
