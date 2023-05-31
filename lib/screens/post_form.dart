63f03eb0134fd50a5a72925c2e940f3837ad326f
margallostephen <spmargallo@gmail.com>
2023-06-01 03:13:36 +0800

    Add button functions

--- lib/screens/post_form.dart
+++ lib/screens/post_form.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:html';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import '../components/app_bar.dart';
 import '../components/style.dart';
@@ -13,10 +15,68 @@ class PostForm extends StatefulWidget {
 class _PostFormState extends State<PostForm> {
   dynamic arguments;
   IconData? icon;
+  bool titleChanged = false, bodyChanged = false;
 
   final taskFormKey = GlobalKey<FormState>();
-  TextEditingController titleController = TextEditingController();
-  TextEditingController bodyController = TextEditingController();
+  TextEditingController titleController = TextEditingController(),
+      bodyController = TextEditingController();
+
+  String url = 'http://localhost:3000/posts';
+
+  Future<http.Response> createPost(String title, String body) async {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+        <String, String>{
+          'title': title,
+          'body': body,
+          'userId': '1',
+        },
+      ),
+    );
+
+    return response;
+  }
+
+  Future<http.Response> updatePost(
+      int postId, String title, String body) async {
+    final response = await http.put(
+      Uri.parse('$url/$postId'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+        <String, String>{
+          'id': '$postId',
+          'title': title,
+          'body': body,
+        },
+      ),
+    );
+
+    return response;
+  }
+
+  Future<http.Response> partialUpdatePost(
+      int postId, String data, String type) async {
+    final response = await http.patch(
+      Uri.parse('$url/$postId'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+        <String, String>{
+          'id': '$postId',
+          type: data,
+        },
+      ),
+    );
+
+    return response;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,6 +107,15 @@ class _PostFormState extends State<PostForm> {
                   child: Column(
                     children: [
                       TextFormField(
+                        onChanged: (value) => setState(
+                          () {
+                            if (value != arguments['name']) {
+                              titleChanged = true;
+                            } else {
+                              titleChanged = false;
+                            }
+                          },
+                        ),
                         controller: titleController,
                         decoration: const InputDecoration(
                           labelStyle: TextStyle(
@@ -65,6 +134,15 @@ class _PostFormState extends State<PostForm> {
                         height: 20,
                       ),
                       TextFormField(
+                        onChanged: (value) => setState(
+                          () {
+                            if (value != arguments['body']) {
+                              bodyChanged = true;
+                            } else {
+                              bodyChanged = false;
+                            }
+                          },
+                        ),
                         controller: titleController,
                         decoration: const InputDecoration(
                           labelStyle: TextStyle(
@@ -92,7 +170,9 @@ class _PostFormState extends State<PostForm> {
                                   Colors.red,
                                 ),
                               ),
-                              onPressed: () {},
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
                               child: const Padding(
                                 padding: EdgeInsets.all(12),
                                 child: Text(
@@ -107,7 +187,64 @@ class _PostFormState extends State<PostForm> {
                           ),
                           Expanded(
                             child: ElevatedButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                if (taskFormKey.currentState!.validate()) {
+                                  bool okayStatus = false;
+
+                                  if (arguments['operation'] == 'Add Post') {
+                                    createPost(
+                                      titleController.text,
+                                      bodyController.text,
+                                    ).then(
+                                      (value) {
+                                        if (value.statusCode == 201) {
+                                          okayStatus = true;
+                                        }
+                                      },
+                                    );
+                                  } else {
+                                    if (titleChanged && bodyChanged) {
+                                      updatePost(
+                                        arguments['post_id'],
+                                        titleController.text,
+                                        bodyController.text,
+                                      ).then(
+                                        (value) {
+                                          if (value.statusCode == 200) {
+                                            okayStatus = true;
+                                          }
+                                        },
+                                      );
+                                    } else {
+                                      String data = '', type = '';
+                                      if (titleChanged) {
+                                        data = titleController.text;
+                                        type = 'title';
+                                      } else {
+                                        if (bodyChanged) {
+                                          data = bodyController.text;
+                                          type = 'body';
+                                        }
+                                      }
+                                      partialUpdatePost(
+                                        arguments['post_id'],
+                                        data,
+                                        type,
+                                      ).then(
+                                        (value) {
+                                          if (value.statusCode == 200) {
+                                            okayStatus = true;
+                                          }
+                                        },
+                                      );
+                                    }
+                                  }
+
+                                  if (okayStatus) {
+                                    Navigator.pop(context);
+                                  }
+                                }
+                              },
                               style: ButtonStyle(
                                 backgroundColor:
                                     MaterialStateProperty.all<Color>(
